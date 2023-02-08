c22f6a0029078d4087464bfe07eb84d82c70f0d0
aleksey <aleksey.bortnik91@gmail.com>
2023-02-08 14:50:09 +0300

    scss loader added

--- src/js/login.js
+++ src/js/login.js
@@ -4,4 +4,3 @@ console.log('login.js');
 //   const wildExpression = /^(?=.{1,100}@)([\w-%+]+(?:\.[\w-%+]+)*)@(?=.{1,58}\.([a-z]{2,10})$)((?:[a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+)\.([a-z]{2,10})$/;
 //   return wildExpression.test(email);
 // }
-
